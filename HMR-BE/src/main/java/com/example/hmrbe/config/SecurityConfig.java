0305b6c1762620ac6c91efb9ab32fa2df787d988
Đỗ Đức Dương <83938501+duongdd141100@users.noreply.github.com>
2023-04-20 14:22:25 +0700

    [BE] Config security

---
+++ HMR-BE/src/main/java/com/example/hmrbe/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.hmrbe.config;
+
+import com.example.hmrbe.constants.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private FilterExceptionHandler filterExceptionHandler;
+
+    @Autowired
+    private JWTAuthFilter jwtAuthFilter;
+
+    @Autowired
+    private UsernamePasswordAuthFilter usernamePasswordAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable()
+                .addFilterBefore(usernamePasswordAuthFilter, BasicAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthFilter.class)
+                .addFilterBefore(filterExceptionHandler, JWTAuthFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers(HttpMethod.POST, Constants.SIGN_IN_API, Constants.SIGN_UP_API, Constants.SIGN_OUT_API).permitAll()
+                            .anyRequest().authenticated();
+                });
+        return http.build();
+    }
+
+}
