e1ee485d82fd2e48a54191261e27c7fb8c4c277e
Đỗ Đức Dương <83938501+duongdd141100@users.noreply.github.com>
2023-04-20 10:50:25 +0700

    [BE] Create constants

---
+++ HMR-BE/src/main/java/com/example/hmrbe/constants/Constants.java
@@ -0,0 +1,13 @@
+package com.example.hmrbe.constants;
+
+public class Constants {
+
+    public static final Integer TOKEN_EXPIRE_MILLISECONDS = 3600 * 1000;
+
+    public static final String SIGN_IN_API = "/api/auth/sign-in";
+
+    public static final String SIGN_UP_API = "/api/auth/sign-up";
+
+    public static final String SIGN_OUT_API = "/api/auth/sign-out";
+
+}
