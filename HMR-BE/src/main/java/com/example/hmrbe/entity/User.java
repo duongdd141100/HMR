c25cdfbbe7fabf2b1ba363fddc6aa2179adad6ab
Đỗ Đức Dương <83938501+duongdd141100@users.noreply.github.com>
2023-04-20 10:49:47 +0700

    [BE] Create entity

---
+++ HMR-BE/src/main/java/com/example/hmrbe/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.hmrbe.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tbl_user")
+@Data
+public class User extends BaseEntity {
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "gender")
+    private Boolean gender;
+
+    @Column(name = "dob")
+    private Date dob;
+
+    @Column(name = "phone_number")
+    private Date phoneNumber;
+
+    @Column(name = "roles")
+    private String roles;
+
+}
