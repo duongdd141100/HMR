60653d60bdb8d248a52080ceb74e89d53f0c235e
Đỗ Đức Dương <83938501+duongdd141100@users.noreply.github.com>
2023-04-21 08:41:49 +0700

    [BE] Create sign in api

--- HMR-BE/src/main/java/com/example/hrmbe/config/UserAuthProvider.java
+++ HMR-BE/src/main/java/com/example/hrmbe/config/UserAuthProvider.java
@@ -4,6 +4,8 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
+import com.example.hrmbe.common.ErrorMessageEnum;
+import com.example.hrmbe.constants.Constants;
 import com.example.hrmbe.entity.User;
 import com.example.hrmbe.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
+import java.util.Date;
 
 @Component
 public class UserAuthProvider {
@@ -29,5 +32,19 @@ public class UserAuthProvider {
         JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secretKey)).build();
         DecodedJWT decodedJWT = verifier.verify(token);
         return new UsernamePasswordAuthenticationToken(authService.findByEmail(decodedJWT.getIssuer()), null, Collections.emptyList());
+    }
+
+    public String createToken(String email) {
+        try {
+            Date now = new Date();
+            Date expiry = new Date(now.getTime() + Constants.TOKEN_EXPIRE_MILLISECONDS);
+            return JWT.create()
+                    .withIssuer(email)
+                    .withIssuedAt(now)
+                    .withExpiresAt(expiry)
+                    .sign(Algorithm.HMAC256(secretKey));
+        } catch (Exception e) {
+            throw new RuntimeException(ErrorMessageEnum.CREATE_TOKEN_FAILED.getCode());
+        }
     }
 }
--- HMR-BE/src/main/java/com/example/hrmbe/controller/AuthController.java
+++ HMR-BE/src/main/java/com/example/hrmbe/controller/AuthController.java
@@ -2,16 +2,15 @@ package com.example.hrmbe.controller;
 
 import com.example.hrmbe.common.BaseResponse;
 import com.example.hrmbe.common.ErrorMessageEnum;
+import com.example.hrmbe.config.UserAuthProvider;
 import com.example.hrmbe.constants.Constants;
 import com.example.hrmbe.entity.User;
 import com.example.hrmbe.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -21,12 +20,35 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private UserAuthProvider userAuthProvider;
+
     @PostMapping("/sign-up")
     public ResponseEntity<BaseResponse<User>> signUp(@RequestBody User user) {
         try {
-            return ResponseEntity.ok(BaseResponse.ok(BaseResponse.ok(authService.save(user))));
+            return ResponseEntity.ok(BaseResponse.ok(authService.save(user)));
         } catch (Exception e) {
             log.error(Constants.SIGN_UP_API + e);
+            return ResponseEntity.badRequest().body(BaseResponse.fail(ErrorMessageEnum.typeOf(e.getMessage()).getMessage()));
+        }
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<BaseResponse<String>> signIn(@AuthenticationPrincipal User user) {
+        try {
+            return ResponseEntity.ok(BaseResponse.ok(userAuthProvider.createToken(user.getEmail())));
+        } catch (Exception e) {
+            log.error(Constants.SIGN_IN_API + e);
+            return ResponseEntity.badRequest().body(BaseResponse.fail(ErrorMessageEnum.typeOf(e.getMessage()).getMessage()));
+        }
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<BaseResponse<String>> me(@AuthenticationPrincipal User user) {
+        try {
+            return ResponseEntity.ok(BaseResponse.ok(user));
+        } catch (Exception e) {
+            log.error(e.getMessage());
             return ResponseEntity.badRequest().body(BaseResponse.fail(ErrorMessageEnum.typeOf(e.getMessage()).getMessage()));
         }
     }
