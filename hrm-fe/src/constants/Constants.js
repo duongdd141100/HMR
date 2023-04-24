834e5151066ac3b79a8e42ada1ebd038864836d6
Đỗ Đức Dương <83938501+duongdd141100@users.noreply.github.com>
2023-04-24 10:28:44 +0700

    [FE] Create Constants

---
+++ hrm-fe/src/constants/Constants.js
@@ -0,0 +1,10 @@
+export const Constants = {
+    api: {
+        host: 'http://localhost:8080',
+        auth :{
+            signIn: '/api/auth/sign-in',
+            signUp: '/api/auth/sign-up',
+            me: '/api/auth/me'
+        }
+    }
+}
--- hrm-fe/src/services/Services.js
+++ hrm-fe/src/services/Services.js
@@ -1,15 +1,17 @@
+import { Constants } from "../constants/Constants";
+
 export const Services = {
     get: async function(url, onSuccess, onFail) {
-        await fetch(url,
+        await fetch(Constants.api.host + url,
             {
                 method: 'GET',
-                headers: {"Authorization": localStorage.getItem("token") || ''}
+                headers: {'Authorization': localStorage.getItem('token') || ''}
             }).then(response => response.json())
             .then(data => {
                 if (data.code === '200') {
                     onSuccess(data);
                 } else {
-                    localStorage.setItem("token", "")
+                    localStorage.setItem('token', '')
                     onFail(data);
                 }
                 return data;
@@ -19,17 +21,17 @@ export const Services = {
             })
     },
     post: async function(url, body, onSuccess, onFail) {
-        await fetch(url,
+        await fetch(Constants.api.host + url,
             {
                 method: 'POST',
-                headers: {"Authorization": localStorage.getItem("token") || ''},
+                headers: {'Authorization': localStorage.getItem('token') || ''},
                 body: body
             }).then(response => response.json())
             .then(data => {
                 if (data.code === '200') {
                     onSuccess(data);
                 } else {
-                    localStorage.setItem("token", "")
+                    localStorage.setItem('', '')
                     onFail(data);
                 }
             }).catch(error => {
