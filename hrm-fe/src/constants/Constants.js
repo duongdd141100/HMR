e5b0bba25f77a489210fa542a0f4e6a78324b211
Đỗ Đức Dương <83938501+duongdd141100@users.noreply.github.com>
2023-05-16 16:43:02 +0700

    [FE] Add header API

--- hrm-fe/src/constants/Constants.js
+++ hrm-fe/src/constants/Constants.js
@@ -1,10 +1,13 @@
 export const Constants = {
     api: {
         host: 'http://localhost:8080',
-        auth :{
+        auth: {
             signIn: '/api/auth/sign-in',
             signUp: '/api/auth/sign-up',
             me: '/api/auth/me'
+        },
+        entries: {
+            headers: '/api/entries/header'
         }
     }
 }
