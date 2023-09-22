6abc2a1d843d29ae447b67f13dc7f714d6af5550
duongdd141100 <duongdd141100@gmail.com>
2023-09-22 10:36:59 +0700

    using configuration properties

---
+++ HMR-BE/src/main/java/com/example/hrmbe/config/AppProperty.java
@@ -0,0 +1,12 @@
+package com.example.hrmbe.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "storage")
+public class AppProperty {
+    String email;
+}
